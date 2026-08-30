5c403b7684611530d42d7b181f587d8bc6372eb9
Foxsia <foxsia29@gmail.com>
2026-08-30 13:01:57 +0200

    add goal scene

---
+++ scripts/Interactable/goal.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		level_complete()
+
+func level_complete():
+	get_tree().change_scene_to_file("")
