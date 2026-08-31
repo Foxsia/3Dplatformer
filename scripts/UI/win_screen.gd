1c69cc7fca37dbd0f30c82405aa2e4f2456e48f2
Foxsia <foxsia29@gmail.com>
2026-08-31 21:54:10 +0200

    fix win screen buttons

--- scripts/UI/win_screen.gd
+++ scripts/UI/win_screen.gd
@@ -4,7 +4,15 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_next_level_pressed():
-	print("Next level")
+	var completed_level = GameManager.current_level
+	
+	GameManager.complete_level(completed_level)
+	
+	var next_level = GameManager.current_level
+	
+	get_tree().change_scene_to_file(
+		"res://scenes/Levels/level_" + str(next_level) + ".tscn"
+	)
 
 func _on_main_menu_pressed():
-	get_tree().change_scene_toa_file("res://scenes/UI/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
